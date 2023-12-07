5c663254ea4bdbf0f1ea2070a1451f43c6ee174a
Stephan Kergomard <webmaster@kergomard.ch>
2023-12-07 18:09:39 +0100

    Test: Fix Tests

--- components/ILIAS/Test/tests/Setup/ilTestSetupAgentTest.php
+++ components/ILIAS/Test/tests/Setup/ilTestSetupAgentTest.php
@@ -18,14 +18,11 @@
 
 namespace Setup;
 
-use ilDatabaseUpdateStepsExecutedObjective;
 use ILIAS\Refinery\Factory as Refinery;
 use ILIAS\Setup\Objective\NullObjective;
-use ILIAS\Test\Setup\ilManScoringSettingsToOwnDbTableMigration;
-use ILIAS\Test\Setup\ilRemoveDynamicTestsAndCorrespondingDataMigration;
-use ILIAS\Test\Setup\ilSeparateQuestionListSettingMigration;
 use ilTestBaseTestCase;
-use ilTestSetupAgent;
+use ILIAS\Test\Setup\ilTestSetupAgent;
+use ILIAS\Setup\ObjectiveCollection;
 
 class ilTestSetupAgentTest extends ilTestBaseTestCase
 {
@@ -38,7 +35,7 @@ class ilTestSetupAgentTest extends ilTestBaseTestCase
     public function testGetUpdateObjective(): void
     {
         $ilTestSetupAgentTest = new ilTestSetupAgent($this->createMock(Refinery::class));
-        $this->assertInstanceOf(ilDatabaseUpdateStepsExecutedObjective::class, $ilTestSetupAgentTest->getUpdateObjective());
+        $this->assertInstanceOf(ObjectiveCollection::class, $ilTestSetupAgentTest->getUpdateObjective());
     }
 
     public function testHasConfig(): void
@@ -63,9 +60,5 @@ class ilTestSetupAgentTest extends ilTestBaseTestCase
     {
         $ilTestSetupAgentTest = new ilTestSetupAgent($this->createMock(Refinery::class));
         $this->assertIsArray($ilTestSetupAgentTest->getMigrations());
-        $this->assertNotEmpty($ilTestSetupAgentTest->getMigrations());
-        $this->assertInstanceOf(ilManScoringSettingsToOwnDbTableMigration::class, $ilTestSetupAgentTest->getMigrations()[0]);
-        $this->assertInstanceOf(ilRemoveDynamicTestsAndCorrespondingDataMigration::class, $ilTestSetupAgentTest->getMigrations()[1]);
-        $this->assertInstanceOf(ilSeparateQuestionListSettingMigration::class, $ilTestSetupAgentTest->getMigrations()[2]);
     }
 }
