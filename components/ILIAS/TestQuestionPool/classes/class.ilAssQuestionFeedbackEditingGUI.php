b0d820f082579533887ca837f26cc2bcf3e1aa06
Stephan Kergomard <webmaster@kergomard.ch>
2024-02-24 09:34:22 +0100

    Test: Update Cloning, Updating & Syncing Questions

--- components/ILIAS/TestQuestionPool/classes/class.ilAssQuestionFeedbackEditingGUI.php
+++ components/ILIAS/TestQuestionPool/classes/class.ilAssQuestionFeedbackEditingGUI.php
@@ -222,7 +222,7 @@ class ilAssQuestionFeedbackEditingGUI
             return false;
         }
 
-        if (!assQuestion::_isWriteable($this->question_obj->getOriginalId(), $this->question_obj->getCurrentUser()->getId())) {
+        if ($this->question_obj->isWriteable()) {
             return false;
         }
 
