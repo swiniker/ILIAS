55882d09959b5cfcd3ccdf8a91e63c359921a47e
Stephan Kergomard <webmaster@kergomard.ch>
2024-02-29 14:38:56 +0100

    Test: More Clean-Up and Issue-Fixing in Questions

--- components/ILIAS/TestQuestionPool/classes/class.assQuestion.php
+++ components/ILIAS/TestQuestionPool/classes/class.assQuestion.php
@@ -2062,7 +2062,7 @@ abstract class assQuestion implements Question
 
     public static function instantiateQuestionGUI(int $question_id): assQuestionGUI
     {
-        //Shouldn't you live in assQuestionGUI, Mister?
+        /** @var ILIAS\DI\Container $DIC */
         global $DIC;
         $ilCtrl = $DIC['ilCtrl'];
         $ilDB = $DIC['ilDB'];
@@ -2071,7 +2071,7 @@ abstract class assQuestion implements Question
         $ilLog = $DIC['ilLog'];
 
         if ($question_id <= 0) {
-            $ilLog->write('Instantiate question called without question id. (instantiateQuestionGUI@assQuestion)', $ilLog->WARNING);
+            $ilLog->warning('Instantiate question called without question id. (instantiateQuestionGUI@assQuestion)');
             throw new InvalidArgumentException('Instantiate question called without question id. (instantiateQuestionGUI@assQuestion)');
         }
 
