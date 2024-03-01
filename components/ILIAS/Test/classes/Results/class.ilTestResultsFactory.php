3b7229a56787a9590bc90c6f3c687c5d24827ea0
Stephan Kergomard <webmaster@kergomard.ch>
2024-03-01 08:40:34 +0100

    Test: Introduce Logging in Manual Feedback

--- components/ILIAS/Test/classes/Results/class.ilTestResultsFactory.php
+++ components/ILIAS/Test/classes/Results/class.ilTestResultsFactory.php
@@ -95,7 +95,7 @@ class ilTestResultsFactory
             $question_gui = $test_obj->createQuestionGUI("", $qid);
             $shuffle_trafo = $this->shuffler->getAnswerShuffleFor($qid, $active_id, $pass_id);
             $question = $question_gui->getObject();
-            $object->setShuffler($shuffle_trafo);
+            $question->setShuffler($shuffle_trafo);
             $question_gui->setObject($question);
 
             $graphical_output = true;
