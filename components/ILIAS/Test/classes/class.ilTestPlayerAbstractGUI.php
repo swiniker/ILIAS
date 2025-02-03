cf50901bba49f2cfe3603f78abc62db0b3f8621e
Matheus Zych <mzych@databay.de>
2025-02-03 09:10:42 +0100

    Redirects user to test tab when test attempt gets finished by administrator

--- components/ILIAS/Test/classes/class.ilTestPlayerAbstractGUI.php
+++ components/ILIAS/Test/classes/class.ilTestPlayerAbstractGUI.php
@@ -215,7 +215,7 @@ abstract class ilTestPlayerAbstractGUI extends ilTestServiceGUI
 
                     if (!$testPassesSelector->openPassExists()) {
                         $this->tpl->setOnScreenMessage('info', $this->lng->txt('tst_pass_finished'), true);
-                        $this->ctrl->redirectByClass([ilRepositoryGUI::class, ilObjTestGUI::class, ilInfoScreenGUI::class]);
+                        $this->ctrl->redirectByClass([ilRepositoryGUI::class, ilObjTestGUI::class, TestScreenGUI::class]);
                     }
                 }
 
